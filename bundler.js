storytouch/ep_webpack

commit 7910f56644e89768c0ec8e1acbaffcf0b0843b17

[fix] Split webpack.config.js files depending on the Etherpad settings
Create different `webpack.config.*.js` files depending of the (current)
4 combinations of settings: with/without minimization; and with/without
CSS bundling. This avoids providing webpack with options it won't need,
like optimizing CSS when we're not minimizing them.

Also revert part of previous commit, and do not split CSS and JS files
into different entries. This change might decrease the performance of a
config with CSS bundling *and* minimizing, but it increases the
performance of the most common config for the dev environment (where
watch mode is usually running): CSS bundling *without* minimizing files.

Improvements (for a bundle of 42 CSS files and 50 JS files, changing a
single CSS or JS file): from 2s to 0.5s.

bundler.js
@@ -6,8 +6,15 @@ var _ = require('underscore');
 var cssBundler = require('./cssBundler');
 
 var JS_INDEX = 'static/js/index.js';
-var CSS_INDEX = 'static/js/indexCSS.js';
-var DEFAULT_WEBPACK_CONFIG = './webpack.config.js';
+var ALL_WEBPACK_CONFIGS = './webpack.config-options.js';
+var DEFAULT_WEBPACK_CONFIG_FILE = './webpack.config-default.js';
+var CONFIG_FILES = [
+  { minify: false, css: false, fileName: DEFAULT_WEBPACK_CONFIG_FILE },
+  { minify: true , css: false, fileName: './webpack.config-withMinify.js' },
+  { minify: false, css: true , fileName: './webpack.config-withCss.js' },
+  { minify: true , css: true , fileName: './webpack.config-withCssAndMinify.js' },
+];
+
 var isProduction = process.env.NODE_ENV !== 'development';
 
 exports.generateBundle = function(pluginParts, settings, done) {
@@ -25,7 +32,7 @@ exports.buildIndexAndGenerateBundle = function(pluginParts, settings, createFile
   var mySettings = settings.ep_webpack || {};
   var partsToBeIgnored = mySettings.ignoredParts || [];
   var shouldBundleCSS = mySettings.bundleCSS;
-  var webpackConfigs = buildWebpackConfigs(mySettings.customWebpackConfigFile, settings);
+  var webpackConfigs = buildWebpackConfigs(mySettings.customWebpackConfigFile, shouldBundleCSS, settings);
 
   var allClientHooks = getAllClientHooks(pluginParts, partsToBeIgnored);
   var jsFilesToBundle = getListOfJsFilesToBundle(allClientHooks);
@@ -35,7 +42,7 @@ exports.buildIndexAndGenerateBundle = function(pluginParts, settings, createFile
   var externalCssFiles = cssBundleProps.externalCssFiles || [];
   var cssHooksToBeSkipped = cssBundleProps.cssHooksToBeSkipped || [];
 
-  generateClientIndexes(jsFilesToBundle, cssFilesToBundle, createFile, function(err) {
+  generateClientIndex(jsFilesToBundle, cssFilesToBundle, createFile, function(err) {
     if (err) {
       done(err);
     } else {
@@ -54,17 +61,29 @@ exports.buildIndexAndGenerateBundle = function(pluginParts, settings, createFile
   });
 }
 
-var buildWebpackConfigs = function(customConfigFile, otherSettings) {
-  var webpackConfigFile = customConfigFile || DEFAULT_WEBPACK_CONFIG;
-  var webpackConfigs = require(webpackConfigFile);
+var buildWebpackConfigs = function(customConfigFile, shouldBundleCSS, otherSettings) {
+  // if no custom webpack file is provided, load default options and get the one
+  // according to the Etherpad settings
+  var configFileName = customConfigFile || getWebpackConfigFileFor(shouldBundleCSS, otherSettings.minify);
+  return require(configFileName);
+}
 
-  // disable minify options on webpack if settings has it turned off
-  if (!otherSettings.minify) {
-    delete webpackConfigs.optimization;
-    delete webpackConfigs.devtool;
-  }
+var getWebpackConfigFileFor = function(shouldBundleCSS, shouldMinify) {
+  var allWebpackConfigs = require(ALL_WEBPACK_CONFIGS);
 
-  return webpackConfigs;
+  // there are multiple configs defined on ALL_WEBPACK_CONFIGS, depending
+  // on the settings. We need to search for the specific one according to
+  // those settings
+  var targetConfig = _(CONFIG_FILES).findWhere({
+    minify: shouldMinify,
+    css: shouldBundleCSS,
+  });
+
+  // use default config if didn't find any. This should not happen, but
+  // let's be extra cautious here
+  var configFileName = (targetConfig || {}).fileName || DEFAULT_WEBPACK_CONFIG_FILE;
+
+  return configFileName;
 }
 
 /*
@@ -145,22 +164,15 @@ var getListOfJsFilesToBundle = function(allClientHooks) {
 
 /*
   Generate a file on ep_webpack/static/js/index.js that imports all plugin hook paths.
-  Do the same for ep_webpack/static/js/indexCSS.js to all CSS files.
-
   Example:
     exports.f0 = require("ep_myplugin1/static/js/index");
     exports.f1 = require("ep_myplugin2/static/js/other_index");
     exports.f2 = require("ep_myplugin2/static/js/index");
     (...)
 */
-var generateClientIndexes = function(jsFilesToBundle, cssFilesToBundle, createFile, done) {
-  generateIndexFile(jsFilesToBundle, JS_INDEX, createFile, function(err) {
-    if (err) {
-      done(err);
-    } else {
-      generateIndexFile(cssFilesToBundle, CSS_INDEX, createFile, done);
-    }
-  });
+var generateClientIndex = function(jsFilesToBundle, cssFilesToBundle, createFile, done) {
+  var allFilesToBundle = [...jsFilesToBundle, ...cssFilesToBundle];
+  generateIndexFile(allFilesToBundle, JS_INDEX, createFile, done);
 }
 
 var generateIndexFile = function(allFilesToBundle, filePath, createFile, done) {

webpack.config-default.js
@@ -1,0 +1,2 @@
+var webpackOptions = require('./webpack.config-options');
+module.exports = webpackOptions.baseConfigs;

.gitignore
@@ -3,5 +3,4 @@ node_modules
 .ep_initialized
 static/dist/*
 static/js/index.js
-static/js/indexCSS.js
 static/js/aceEditorCSS.js

webpack.config-options.js
@@ -1,19 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 var isProduction = process.env.NODE_ENV !== 'development';
-var JS_FILENAME = `js/[name]${isProduction ? '-[hash]': ''}.js`;
+var JS_FILENAME = `js/index${isProduction ? '-[hash]': ''}.js`;
 var CSS_FILENAME = `css/all${isProduction ? '-[hash]': ''}.css`;
 
-module.exports = {
+var baseConfigs = {
   mode: isProduction ? 'production' : 'development',
-  entry: {
-    index: path.resolve(__dirname, 'static/js/index.js'),
-    css: path.resolve(__dirname, 'static/js/indexCSS.js'),
-  },
+  entry: path.resolve(__dirname, 'static/js/index.js'),
 
   output: {
     filename: JS_FILENAME,
@@ -36,23 +34,7 @@ module.exports = {
       // require it when they use that variable
       autocomp: ['ep_autocomp/static/js/index', 'autocomp'],
     }),
-
-    // Bundle CSS into a single file
-    new MiniCssExtractPlugin({
-      filename: CSS_FILENAME,
-    }),
-    // Minify CSS files
-    new OptimizeCssAssetsPlugin(),
   ],
-
-  // minimize
-  // Other options: https://webpack.js.org/configuration/devtool/
-  //   - 'inline-cheap-source-map': 12s, 1.8Mb
-  //   - 'cheap-source-map':        12s, 0.8Mb
-  devtool: 'cheap-source-map',
-  optimization: {
-    minimize: true,
-  },
 
   // watch mode (disabled on production)
   watchOptions: isProduction ? {} : {
@@ -69,7 +51,33 @@ module.exports = {
         test: /jquery.+\.js$/,
         use: 'ep_webpack/node_modules/imports-loader?define=>false',
       },
-      // Bundle CSS + SASS
+    ],
+  },
+};
+
+// configs specific for when we need to minify files
+var minifyConfigs = {
+  // Other options: https://webpack.js.org/configuration/devtool/
+  //   - 'inline-cheap-source-map': 12s, 1.8Mb
+  //   - 'cheap-source-map':        12s, 0.8Mb
+  devtool: 'cheap-source-map',
+  optimization: {
+    minimize: true,
+  },
+};
+
+// configs specific for when we need to bundle CSS files
+var cssConfigs = {
+  // Bundle CSS into a single file
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: CSS_FILENAME,
+    }),
+  ],
+
+  // Bundle CSS + SASS
+  module: {
+    rules: [
       {
         test: /\.s?css$/,
         use: [
@@ -80,4 +88,19 @@ module.exports = {
       },
     ],
   },
+};
+
+// configs specific for when we need to bundle CSS + minify files
+var cssAndMinifyConfigs = {
+  // Minify CSS files
+  plugins: [
+    new OptimizeCssAssetsPlugin(),
+  ],
+};
+
+module.exports = {
+  default: baseConfigs,
+  withMinify: merge(baseConfigs, minifyConfigs),
+  withCss: merge(baseConfigs, cssConfigs),
+  withMinifyAndCss: merge(baseConfigs, cssConfigs, minifyConfigs, cssAndMinifyConfigs),
 };

webpack.config-withCss.js
@@ -1,0 +1,2 @@
+var webpackOptions = require('./webpack.config-options');
+module.exports = webpackOptions.withCss;

webpack.config-withMinify.js
@@ -1,0 +1,2 @@
+var webpackOptions = require('./webpack.config-options');
+module.exports = webpackOptions.withMinify;

webpack.config-withMinifyAndCss.js
@@ -1,0 +1,2 @@
+var webpackOptions = require('./webpack.config-options');
+module.exports = webpackOptions.withMinifyAndCss;

commit c5a9c4bbcdb02f582eb33ab5a041851a4c8c30ab

[fix:test] Code cleanup & adjustment to fix tests
It looks like commit 7910f566 broke some tests, so let's fix them.

bundler.js
@@ -6,7 +6,6 @@ var _ = require('underscore');
 var cssBundler = require('./cssBundler');
 
 var JS_INDEX = 'static/js/index.js';
-var ALL_WEBPACK_CONFIGS = './webpack.config-options.js';
 var DEFAULT_WEBPACK_CONFIG_FILE = './webpack.config-default.js';
 var CONFIG_FILES = [
   { minify: false, css: false, fileName: DEFAULT_WEBPACK_CONFIG_FILE },
@@ -69,14 +68,13 @@ var buildWebpackConfigs = function(customConfigFile, shouldBundleCSS, otherSetti
 }
 
 var getWebpackConfigFileFor = function(shouldBundleCSS, shouldMinify) {
-  var allWebpackConfigs = require(ALL_WEBPACK_CONFIGS);
-
-  // there are multiple configs defined on ALL_WEBPACK_CONFIGS, depending
+  // there are multiple configs defined on CONFIG_FILES, depending
   // on the settings. We need to search for the specific one according to
   // those settings
   var targetConfig = _(CONFIG_FILES).findWhere({
-    minify: shouldMinify,
-    css: shouldBundleCSS,
+    // Use `!!` to force values to be booleans
+    minify: !!shouldMinify,
+    css: !!shouldBundleCSS,
   });
 
   // use default config if didn't find any. This should not happen, but

webpack.config-default.js
@@ -1,2 +1,2 @@
 var webpackOptions = require('./webpack.config-options');
-module.exports = webpackOptions.baseConfigs;
+module.exports = webpackOptions.default;

static/tests/backend/specs/bundler.spec.js
@@ -48,6 +48,7 @@ describe('Plugin Bundler', function() {
 
     context('when settings has "minify" flag turned on', function() {
       before(function(done) {
+        this.timeout(5000);
         subject({ minify: true }, done);
       });
 